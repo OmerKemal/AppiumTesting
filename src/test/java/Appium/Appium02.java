1a43e32fadea86a8e4243fdc8cf6b7e5ba2db9cc
Omer Kemal Gultekin <Omerkemalgultekin@gmail.com>
2023-10-05 15:54:52 +0100

    Appium05.10

--- src/test/java/Appium/Appium02.java
+++ src/test/java/Appium/Appium02.java
@@ -3,16 +3,17 @@ package Appium;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Appium01 {
+public class Appium02 {
     @Test
-    public void test() throws MalformedURLException {
+    public void test() throws MalformedURLException, InterruptedException {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
@@ -31,6 +32,18 @@ public class Appium01 {
 
        // ESKI===>>> AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wb/hub"),capabilities);
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723"),capabilities);
+
+        driver.findElementById("com.android.permissioncontroller:id/continue_button").click();
+        Thread.sleep(3000);
+        WebElement okButton=driver.findElementById("android:id/button1");
+        okButton.click();
+
+
+       //MobileElement homescreenTitle = driver.findElementById("");
+        Thread.sleep(3000);
+        MobileElement addGestureButton = driver.findElementById("com.davemac327.gesture.tool:id/addButton");
+        Thread.sleep(3000);
+        Assert.assertTrue(addGestureButton.isDisplayed());
 
     }
 }
